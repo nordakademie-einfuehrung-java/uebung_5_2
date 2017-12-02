6be40a94dc65b5aa50ee8634f6b7e34dcb05347a
Ben6363 <15352@nordakademie.de>
2017-12-02 17:36:16 +0100

    Moin Herr Kimminich, dasselbe Problem wie bei uebung_5_1

---
+++ src/uebung_5_2/Arrayssortieren.java
@@ -0,0 +1,34 @@
+package uebung_5_2;
+
+import Zufall;
+
+public class Arrayssortieren {
+	public static void main(String[] args) {
+		
+int min;
+		
+		int array [] = Zufall.erzeugeIntArray(10);
+		for (int i : array){
+			System.out.println(i);
+		}
+		for (int index = 0; index < array.length; index++){
+			
+				
+			for (int i = 0; i < array.length; i++){
+				if (array[i]>array[index]){
+					min = array[index];
+					
+					array[index] = array[i];
+					array[i] = min;
+					
+				}
+			}
+		}
+		System.out.println();
+		for (int i : array){
+			System.out.println(i);
+		}
+		
+		
+	}
+}
